23ea008c6e391d70288b89e307f53f2fcfa3597e
Yuttasak Pannawat <yuttasakcom@gmail.com>
2019-03-01 21:15:56 +0700

    create qraphql structrue

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
+const { resolve } = require('path')
+const { readFileSync } = require('fs')
 const express = require('express')
-const { ApolloServer, gql } = require('apollo-server-express')
+const { ApolloServer } = require('apollo-server-express')
 const app = express()
 const PORT = process.env.PORT || '4000'
 
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`
+const filePath = resolve(__dirname, './graphql/schema/typeDefs.gql')
+const typeDefs = readFileSync(filePath, 'utf-8')
 
-const resolvers = {
-  Query: {
-    hello: () => 'Hello, GraphQL',
-  },
-}
+const resolvers = require('./graphql/resolvers/resolver')
 
 const server = new ApolloServer({ typeDefs, resolvers })
 server.applyMiddleware({ app })
