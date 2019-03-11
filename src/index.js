cf427911c96b2ee5296c6f71460a710094ee99da
Yuttasak Pannawat <yuttasakcom@gmail.com>
2019-03-11 21:25:01 +0700

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 const express = require('express')
-const { ApolloServer, gql } = require('apollo-server-express')
+const { ApolloServer } = require('apollo-server-express')
+
 const app = express()
 const PORT = process.env.PORT || '4000'
 
 const models = require('./models')
-
 const typeDefs = require('./typeDefs')
-
 const resolvers = require('./resolvers')
 
 const server = new ApolloServer({ typeDefs, resolvers, context: { models } })
--- src/typeDefs/index.js
+++ src/typeDefs/index.js
@@ -1,6 +1,5 @@
 const userSchema = require('./user')
 const carSchema = require('./car')
-
 const defaultSchema = require('./default')
 
 module.exports = [defaultSchema, userSchema, carSchema]
